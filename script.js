ac874bb37f46c0da10e69ef1a8429d2d6da4d22b
anthony <anthonyreyes11@gmail.com>
2022-06-10 13:29:33 -0400

    declaramos score y time , y formulamos time +3 y score +1

--- script.js
+++ script.js
@@ -22,24 +22,28 @@ const words = [
 // variables globales
 let time = 10;
 let score = 0;
-let palabraAleatoria = randomWords();
+let palabraAleatoria;
 
 
 // etiquetas HTML
 const h1 = document.querySelector("#randomWord");
 const input = document.querySelector('#text');
+const timeSpan = document.querySelector('#timeSpan')
+const scoreSpan = document.querySelector('#score')
 
 // funciones
 function randomWords() {
   const indice = Math.floor(Math.random() * words.length);
   return words[indice];
-
 }
 
 function addToDOM() {
+  palabraAleatoria= randomWords();
   h1.textContent = palabraAleatoria;
-
-
+  // ingresar el tiempo restante en la pagina
+  span_time.textContent = time;
+  // ingresar el puntaje en la pagina
+  span_score.textContent = score;
 }
 
 // inicio del juego
@@ -50,15 +54,18 @@ input.addEventListener('keyup', function (event) {
 
   if (event.keyCode == 13) {
     console.log('Esto es un enter');
-    
+
     let palabraingresada = input.value;
 
     if (palabraingresada == palabraAleatoria) {
       console.log('Ganaste')
-    
+      //tiempo aumente 3 segundos 
+      time += 3;
+      score += 1;
 
     }
   }
 
-
 });
+
+
