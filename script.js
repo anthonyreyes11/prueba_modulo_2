894e17d5c01d5db8484b4d122419d2ff340e24ed
anthony <anthonyreyes11@gmail.com>
2022-06-09 20:54:29 -0400

    se agregar estilo al container y se el evento que detecta el enter

--- script.js
+++ script.js
@@ -1,46 +1,58 @@
 const words = [
-    'californication',
-    'plataforma5',
-    'black',
-    'summer',
-    'flea',
-    'aeroplane',
-    'peppers',
-    
-    'unlimited',
-    'arcadium',
-    'love',
-    'getaway',
-    'stadium',
-    'quixoticelixer',
-    'quarter',
-    'snow',
-    'dylan',
-    'zephyr',
-    'funky',
-    'chili'
+  'californication',
+  'plataforma5',
+  'black',
+  'summer',
+  'flea',
+  'aeroplane',
+  'peppers',
+
+  'unlimited',
+  'arcadium',
+  'love',
+  'getaway',
+  'stadium',
+  'quixoticelixer',
+  'quarter',
+  'snow',
+  'dylan',
+  'zephyr',
+  'funky',
+  'chili'
 ];
 // variables globales
-let palabraAleatoria; 
+let palabraAleatoria;
 let time = 10;
-let score= 0 ;
+let score = 0;
 
 // etiquetas HTML
-const h1= document.querySelector("#randomWord")
+const h1 = document.querySelector("#randomWord");
+const input = document.querySelector('#text');
 
 // funciones
 function randomWords() {
-    const indice= Math.floor(Math.random() * words.length)
-    return words[indice]
+  const indice = Math.floor(Math.random() * words.length);
+  return words[indice];
 
-}    
+}
 
 function addToDOM() {
-    palabraAleatoria = randomWords();
-    h1.textContent= palabraAleatoria
+  palabraAleatoria = randomWords();
+  h1.textContent = palabraAleatoria;
 }
 
 // inicio del juego
 addToDOM()
 
 
+input.addEventListener('keyup', function (event) {
+
+  if (event.keyCode == 13) {
+    console.log('Esto es un enter');
+
+
+
+  }
+
+
+});
